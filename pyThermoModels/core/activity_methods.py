b3d37c5445bcf58bf697c055c10c93451a7b52bf
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-10-09 17:30:48 -0400

    upd

--- pyThermoModels/core/activity_methods.py
+++ pyThermoModels/core/activity_methods.py
@@ -1,1 +1,12 @@
 # import libs
+import logging
+from typing import Any, Dict, Literal, List
+from pythermodb_settings.models import Component, Temperature, Pressure
+from pythermodb_settings.utils import set_component_id
+from pyThermoLinkDB.models import ModelSource
+# local
+from ..docs import ThermoModelCore
+from ..utils import set_feed_specification
+
+# NOTE: logger
+logger = logging.getLogger(__name__)
