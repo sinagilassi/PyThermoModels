23d6a167c1f905503cbf1986dd28431930a22aae
sina.gilassi <sina.gilassi@gmail.com>
2026-09-16 01:34:33 -0400

    feat: Add __init__.py files for Lee-Kesler and mixing modules to define public API

---
+++ pyThermoModels/eos/lee_kesler/__init__.py
@@ -0,0 +1,9 @@
+from .model import LeeKeslerResult, lee_kesler_pure
+from .mixture import LeeKeslerMixtureResult, lee_kesler_ploecker_mixture
+
+__all__ = [
+    "LeeKeslerResult",
+    "lee_kesler_pure",
+    "LeeKeslerMixtureResult",
+    "lee_kesler_ploecker_mixture",
+]
---
+++ pyThermoModels/eos/mixing/__init__.py
@@ -0,0 +1,9 @@
+from .base import MixingResult
+from .classical import classical_quadratic_mixing
+from .wong_sandler import wong_sandler_pr_nrtl_mixing
+
+__all__ = [
+    "MixingResult",
+    "classical_quadratic_mixing",
+    "wong_sandler_pr_nrtl_mixing",
+]
