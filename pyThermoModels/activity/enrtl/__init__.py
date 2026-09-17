4c6dda9d5d37f9e73644d1cdedc9da75a04de36a
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-09-17 02:24:27 -0400

    feat: Add tau temperature derivative to ENRTL module exports

--- pyThermoModels/activity/enrtl/__init__.py
+++ pyThermoModels/activity/enrtl/__init__.py
@@ -1,10 +1,11 @@
-from .model import ENRTL
+﻿from .model import ENRTL
 from .component_adapter import ENRTLComponentAdapter
 from .core import ActivityBasis, CompositionRepresentation, ENRTLCore
 from .local_composition import ENRTLFormulation, ENRTLLocalComposition
 from .long_range import ENRTLLongRange
 from .parameter_builder import ENRTLParameterBuilder
 from .parameter_core import ENRTLParameterCore
+from .derivatives import tau_temperature_derivative
 
 __all__ = [
     "ENRTL",
@@ -17,4 +18,5 @@ __all__ = [
     "ENRTLLongRange",
     "ENRTLParameterBuilder",
     "ENRTLParameterCore",
+    "tau_temperature_derivative",
 ]
